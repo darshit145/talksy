24c89061acb17b5c4d5f08a068e8840f2ceefef1
darshit145 <dfachara7@gmail.com>
2025-02-13 13:54:57 +0530

    initial push

---
+++ lib/util/images.dart
@@ -0,0 +1,30 @@
+class Images{
+  static const splashBackground="assets/images/img.png";
+  static const appLogo="assets/images/app_logo.png";
+  static const onboarding_1="assets/images/onboarding_1.png";
+  static const onboarding_2="assets/images/onboarding_2.png";
+  static const onboarding_3="assets/images/onboarding_3.png";
+
+
+  //svg
+  static const lockIcon="assets/icon/lock_icon.svg";
+  static const userShild="assets/icon/user_shild.svg";
+  static const fingureprintIcon="assets/icon/fingureprint_icon.svg";
+  static const messageTimer="assets/icon/message_timer.svg";
+  static const shildIcon="assets/icon/shild_icon.svg";
+  static const messagingIcon="assets/icon/messaging_icon.svg";
+  static const callIcon="assets/icon/call_icon.svg";
+  static const groupChatIcon="assets/icon/group_chat_icon.svg";
+  static const energyIcon="assets/icon/energy_icon.svg";
+  static const earthIcon="assets/icon/earth_icon.svg";
+  static const bottomBarChatIcon="assets/icon/bottom_bar_chat_icon.svg";
+  static const bottomBarContactIcon="assets/icon/bottom_bar_contact_icon.svg";
+  static const bottomBarSettingIcon="assets/icon/bottom_bar_setting_icon.svg";
+  static const colorPalletIcon="assets/icon/color_pallet_icon.svg";
+  static const qnaIcon="assets/icon/qna_icon.svg";
+  static const videoCallIcon="assets/icon/video_call_icon.svg";
+  static const sendIcon="assets/icon/send_icon.svg";
+  static const arrowRight="assets/icon/arrow_right.svg";
+  static const pancilIcon="assets/icon/pancil_icon.svg";
+}
+//Pacifico-Regular.ttf
